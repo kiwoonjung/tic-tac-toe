f426738ecf266a09f1bfa6d851836d247cef7317
kiwoonjung <kiwoon0627@gmail.com>
2024-05-15 23:34:25 -0700

    20240515 23:33 conditional content & a suboptimal way of updating state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,13 +4,16 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
+    // setIsEditing(isEditing ? false : true);
     setIsEditing(!isEditing);
   }
 
   let playerName = <span className="playr-name">{name}</span>;
+  // let btnCaption = "Edit";
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
+    // btnCaption = "Save";
   }
 
   return (
@@ -19,7 +22,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
